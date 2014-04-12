3b98b0b0b98a5e92ad153b5e8eb37d2cae4dbbad
Sam Whitehall <me@samwhitehall.com>
2014-04-12 22:33:34 +0100

    docstrings added to notifiers

--- notifiers.py
+++ notifiers.py
@@ -1,10 +1,17 @@
+''' notifiers.py : contains separated code for actually performing the 
+notification itself.'''
+
 import sys
 
 class TerminalBell:
+    '''Default choice to print an audible bell character to stdout. Works in
+    terminal IPython and IPython QT, but not in IPython Notebook.'''
+
     def ping(self, expr, out):
         sys.stdout.write('\a')
 
 class NSBeep:
+    '''System beep (OS X only).'''
     def ping(self, expr, out):
         try:
             from AppKit import NSBeep
@@ -15,6 +22,7 @@ class NSBeep:
                 "not on OS X, or are on an old version without PyObjC")
 
 class OSXNotificationCentre:
+    '''Send a full notification to OS X Notification Centre (OS X 10.8+)'''
     def ping(self, expr, out, sound=True):
         import Foundation, AppKit, objc # TODO: graceful error handling
 
