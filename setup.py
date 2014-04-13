59ffc3c1b77de400723930794f296a8a2349949e
Sam Whitehall <me@samwhitehall.com>
2014-04-13 22:20:44 +0100

    full installer

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='IPythonBell',
+    version='0.9',
+    packages=['ipythonbell',],
+    license='MIT',
+    author='Sam Whitehall',
+    author_email='me@samwhitehall.com',
+    description='Python line & cell magic to notify the programmer when a '
+        'line/cell has completed execution',
+    install_requires=[
+        'IPython >= 1.00',
+    ]
+)
