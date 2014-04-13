56acedf93e7660fbb19bfc49248e9041db9ebb01
Sam Whitehall <me@samwhitehall.com>
2014-04-13 23:27:43 +0100

    PyPI compliance

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from distutils.core import setup
 
 setup(
     name='IPythonBell',
-    version='0.9',
+    version='0.9.1',
     packages=['ipybell',],
     license='MIT',
     author='Sam Whitehall',
     author_email='me@samwhitehall.com',
+    url='http://www.github.com/samwhitehall/ipython-bell',
     description='Python line & cell magic to notify the programmer when a '
         'line/cell has completed execution',
     requires=[
